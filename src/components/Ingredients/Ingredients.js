c83616c818d600f9fdcb45e4bcc82c3fedb451e4
Aydan Smith <aydanmjsmith@gmail.com>
2021-02-15 11:30:40 +0000

    Initial commit

---
+++ src/components/Ingredients/Ingredients.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
+import Search from './Search';
+
+function Ingredients() {
+  const [ingredientList, setIngredientList] = useState([]); 
+
+  const addIngredientHandler = (ingredient) => {
+    setIngredientList([...ingredientList, { id:Math.random(), ...ingredient }]);
+  };
+
+  const removeIngredientHandler = (ingredientId) => {
+    const updIngredientList = ingredientList
+      .filter( ingredient => {
+        if (ingredient.id === ingredientId) {
+          return false;
+        }
+        return true;
+      });
+
+    setIngredientList(updIngredientList);
+  }
+
+  return (
+    <div className="App">
+      <IngredientForm onAddItem={addIngredientHandler}/>
+
+      <section>
+        <Search />
+        <IngredientList 
+          onRemoveItem={removeIngredientHandler}
+          ingredients={ingredientList} />
+      </section>
+    </div>
+  );
+}
+
+export default Ingredients;
